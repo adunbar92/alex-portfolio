434f1b06eaaa89e42f08238fac89641ecefb94e1
Alex Dunbar <alexandrea.dunbar92@gmail.com>
2026-08-31 00:26:57 -0700

    Enhance contact form validation and styling; add error handling for empty fields and adjust input width

--- Milestone 4/script.js
+++ Milestone 4/script.js
@@ -4,3 +4,64 @@ const menu = document.querySelector(".navigation");
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
 });
+
+const contactForm = document.querySelector(".contact")
+const firstName = document.querySelector("#first-name")
+const firstNameEmpty = document.querySelector("#first-name-empty")
+
+contactForm.addEventListener("submit", (event) => { 
+if(firstName.value === "") {
+    event.preventDefault();
+    firstNameEmpty.classList.add("show-error");
+}
+});
+
+firstName.addEventListener("input", () => {
+firstNameEmpty.classList.remove("show-error");
+
+});
+
+const lastName = document.querySelector("#last-name")
+const lastNameEmpty = document.querySelector("#last-name-empty")
+
+contactForm.addEventListener("submit", (event) => { 
+if(lastName.value === "") {
+    event.preventDefault();
+    lastNameEmpty.classList.add("show-error");
+}
+});
+
+lastName.addEventListener("input", () => {
+lastNameEmpty.classList.remove("show-error");
+
+});
+
+const email = document.querySelector("#email")
+const emailInvalid = document.querySelector("#email-invalid")
+
+contactForm.addEventListener("submit", (event) => { 
+if(email.value === "") {
+    event.preventDefault();
+    emailInvalid.classList.add("show-error");
+}
+});
+
+email.addEventListener("input", () => {
+emailInvalid.classList.remove("show-error");
+
+});
+
+const message = document.querySelector("#message")
+const messageEmpty = document.querySelector("#message-empty")
+
+contactForm.addEventListener("submit", (event) => { 
+if(message.value === "") {
+    event.preventDefault();
+    messageEmpty.classList.add("show-error");
+}
+});
+
+message.addEventListener("input", () => {
+messageEmpty.classList.remove("show-error");
+
+});
